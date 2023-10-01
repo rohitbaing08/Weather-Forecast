a600e83c8310d16e597a27592da4617f9b1ef8ca
rohitbaing08 <rohitbaing456@gmail.com>
2023-10-01 23:49:52 +0530

    Code fixed

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -22,25 +22,24 @@ const Card = () => {
   const [cityInput, setCityInput] = useState("");
   const [loading, setLoading] = useState(true);
   useEffect(() => {
-
     getData();
   }, []);
 
   const getCityData = () => {
-    console.log(cityInput);
     try {
       axios
         .get(
           `http://api.worldweatheronline.com/premium/v1/weather.ashx?key=d7b1eb06630f4f8798a174343231509&q=${cityInput}&num_of_days=2&format=json`
         )
         .then((res) => {
-          console.log(res);
-          setData(res.data.data.current_condition[0]);
-          setDesc(res.data.data.current_condition[0].weatherDesc[0].value);
-          setData1(res.data.data.weather[0].hourly);
-          setDate(res.data.data.weather[0].date);
-          setCity(cityInput);
-          setLoading(false);
+          if (!res.data.data.error) {
+            setData(res.data.data.current_condition[0]);
+            setDesc(res.data.data.current_condition[0].weatherDesc[0].value);
+            setData1(res.data.data.weather[0].hourly);
+            setDate(res.data.data.weather[0].date);
+            setCity(cityInput);
+            setLoading(false);
+          }
         });
     } catch (error) {
       console.log(error);
@@ -51,24 +50,32 @@ const Card = () => {
     if ("geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition(async function (position) {
         const { latitude, longitude } = position.coords;
-        axios
-          .get(
-            `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=dd2a410fd154437dac9e7127a9107d33`
-          )
-          .then((res) => {
-            setCity(`${res.data.results[0].components.city},`);
-          });
-        axios
-          .get(
-            `http://api.worldweatheronline.com/premium/v1/weather.ashx?key=d7b1eb06630f4f8798a174343231509&q=${latitude},${longitude}&num_of_days=2&format=json`
-          )
-          .then((res) => {
-            setData(res.data.data.current_condition[0]);
-            setDesc(res.data.data.current_condition[0].weatherDesc[0].value);
-            setData1(res.data.data.weather[0].hourly);
-            setDate(res.data.data.weather[0].date);
-            setLoading(false);
-          });
+        try {
+          axios
+            .get(
+              `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=dd2a410fd154437dac9e7127a9107d33`
+            )
+            .then((res) => {
+              setCity(`${res.data.results[0].components.city},`);
+            });
+          axios
+            .get(
+              `http://api.worldweatheronline.com/premium/v1/weather.ashx?key=d7b1eb06630f4f8798a174343231509&q=${latitude},${longitude}&num_of_days=2&format=json`
+            )
+            .then((res) => {
+              if (res.status == 200) {
+                setData(res.data.data.current_condition[0]);
+                setDesc(
+                  res.data.data.current_condition[0].weatherDesc[0].value
+                );
+                setData1(res.data.data.weather[0].hourly);
+                setDate(res.data.data.weather[0].date);
+                setLoading(false);
+              }
+            });
+        } catch (error) {
+          console.log(error);
+        }
       });
     }
   };
@@ -123,8 +130,6 @@ const Card = () => {
                   </div>
                 </form>
               </div>
-
-              <div className="iconBig"></div>
               <div className="date">{`Date : ${date || ""}`}</div>
               <table className="weather-history-table tables">
                 <thead>
@@ -144,7 +149,10 @@ const Card = () => {
                     {data1.map((entry) => (
                       <td key={data1.indexOf(entry)} className="border">
                         <div className="table-img">
-                          <img src={entry.weatherIconUrl[0].value || ""} alt="" />
+                          <img
+                            src={entry?.weatherIconUrl[0]?.value || ""}
+                            alt=""
+                          />
                         </div>
                       </td>
                     ))}
@@ -166,7 +174,7 @@ const Card = () => {
                         key={data1.indexOf(entry)}
                         className="white temp border description"
                       >
-                        {entry.weatherDesc[0].value || ""}
+                        {entry?.weatherDesc[0]?.value || ""}
                       </td>
                     ))}
                   </tr>
